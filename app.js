ef47cd489a3a41bc022f80bfb4f78372d1b30775
pgg-dev <park.geumgyu@gmail.com>
2020-06-10 00:44:29 +0900

    add to event

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const table = document.getElementsByTagName("table");
+const board = document.getElementById("board");
 
 for (let i = 0; i < 19; i++) {
-  table[0].appendChild(document.createElement("tr"));
+  const row = board.appendChild(document.createElement("div"));
+  row.className = "row";
+  row.id = i;
   for (let j = 0; j < 19; j++) {
-    const tr = document.getElementsByTagName("tr");
-    tr[i].className = "tr";
-    tr[i].appendChild(document.createElement("td"));
+    const col = document.createElement("div");
+    col.className = "col";
+    col.id = j;
+    row.appendChild(col);
   }
+}
+
+const cols = document.getElementsByClassName("col");
+
+let arr = [];
+let user = true;
+
+function handleClick(e) {
+  user = !user;
+  const row = e.target.parentNode.id;
+  const col = e.target.id;
+  arr[row] = [];
+  if (user) {
+    arr[row][col] = 1;
+    e.target.style = "background: black; border-radius: 50%;";
+  } else {
+    arr[row][col] = 2;
+    e.target.style = "background: white; border-radius: 50%;";
+  }
+}
+
+if (cols) {
+  Array.from(cols).forEach((col) => col.addEventListener("click", handleClick));
 }
