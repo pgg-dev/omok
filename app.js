ef1dfafd4badd12607e3780f004dc74984891feb
pgg-dev <park.geumgyu@gmail.com>
2020-06-12 01:54:43 +0900

    바둑돌 중복 및 가로/세로 체크

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const board = document.getElementById("board");
+const cols = document.getElementsByClassName("col");
+
+let user = true;
+
+let rowCount = 0;
+let rowValue = null;
+let rowNext = null;
+
+let colCount = 0;
+let colValue = null;
+let colNext = null;
+
+let crossCount = 0;
+let crossValue = null;
+let crossNext = null;
+
+var arr = Array(19)
+  .fill(null)
+  .map(() => Array());
 
 for (let i = 0; i < 19; i++) {
   const row = board.appendChild(document.createElement("div"));
@@ -12,23 +31,54 @@ for (let i = 0; i < 19; i++) {
   }
 }
 
-const cols = document.getElementsByClassName("col");
+const check = () => {
+  for (let i = 0; i < 18; i++) {
+    rowCount = 0;
+    colCount = 0;
 
-let arr = [];
-let user = true;
+    if (crossValue && crossNext && crossValue === crossNext) {
+      crossCount = crossCount + 1;
+    }
+
+    for (let j = 0; j < 18; j++) {
+      colValue = arr[j][i];
+      colNext = arr[j + 1][i];
+
+      if (colValue && colNext && colValue === colNext) {
+        colCount = colCount + 1;
+      }
+
+      rowValue = arr[i][j];
+      rowNext = arr[i][j + 1];
+      if (rowValue && rowNext && rowValue === rowNext) {
+        rowCount = rowCount + 1;
+      }
+
+      if (colCount === 4 || rowCount === 4 || crossCount === 4) {
+        setTimeout(() => alert("!!!!!!!!!!"), 1);
+        colCount = 0;
+        rowCount = 0;
+      }
+    }
+  }
+};
 
 function handleClick(e) {
-  user = !user;
   const row = e.target.parentNode.id;
   const col = e.target.id;
-  arr[row] = [];
-  if (user) {
-    arr[row][col] = 1;
-    e.target.style = "background: black; border-radius: 50%;";
-  } else {
-    arr[row][col] = 2;
-    e.target.style = "background: white; border-radius: 50%;";
+
+  if (!arr[row][col]) {
+    if (user) {
+      arr[row][col] = 1;
+      e.target.style = "background: black; border-radius: 50%;";
+    } else {
+      arr[row][col] = 2;
+      e.target.style = "background: white; border-radius: 50%;";
+    }
+    user = !user;
   }
+
+  check();
 }
 
 if (cols) {
