27c1ca14b885d748f82277e01e53495b8d181c1f
pgg-dev <park.geumgyu@gmail.com>
2020-06-14 09:36:39 +0900

    오목 로직 수정

--- app.js
+++ app.js
@@ -2,20 +2,16 @@ const board = document.getElementById("board");
 const cols = document.getElementsByClassName("col");
 
 let user = true;
+let game = true;
 
-let rowCount = 0;
-let rowValue = null;
-let rowNext = null;
-
-let colCount = 0;
-let colValue = null;
-let colNext = null;
+let xIndex = [];
+let yIndex = [];
 
 let crossCount = 0;
-let crossValue = null;
-let crossNext = null;
+let xCount = 0;
+let yCount = 0;
 
-var arr = Array(19)
+let arr = Array(19)
   .fill(null)
   .map(() => Array());
 
@@ -31,35 +27,66 @@ for (let i = 0; i < 19; i++) {
   }
 }
 
-const check = () => {
-  for (let i = 0; i < 18; i++) {
-    rowCount = 0;
-    colCount = 0;
+const indexCheck = (x, y) => {
+  xIndex = [];
+  yIndex = [];
+  for (let i = x + 4; i > x - 5; i--) {
+    if (x !== i && 0 <= i && i < 19) {
+      xIndex.push(i);
+    }
+  }
+  for (let j = y + 4; j > y - 5; j--) {
+    if (y !== j && 0 <= j && j < 19) {
+      yIndex.push(j);
+    }
+  }
 
-    if (crossValue && crossNext && crossValue === crossNext) {
+  console.log(`[${x}, ${y}]`);
+  console.log("xIndex");
+  console.log(xIndex);
+  console.log("yIndex");
+  console.log(yIndex);
+
+  valueCheck(x, y);
+};
+
+const valueCheck = (x, y) => {
+  xCount = 0;
+  yCount = 0;
+  crossCount = 0;
+
+  xIndex.forEach((i) => {
+    if (arr[x][y] === arr[i][y]) {
+      xCount = xCount + 1;
+    }
+  });
+
+  yIndex.forEach((j) => {
+    if (arr[x][y] === arr[x][j]) {
+      yCount = yCount + 1;
+    }
+  });
+
+  const a = xIndex.length > yIndex.length ? yIndex : xIndex;
+
+  for (let i = 0; i < a.length; i++) {
+    const b = xIndex[i];
+    const c = yIndex[i];
+
+    let reverseArr = yIndex;
+    reverseArr = yIndex.reverse();
+
+    const d = reverseArr[i];
+    if (arr[x][y] === arr[b][c]) {
       crossCount = crossCount + 1;
     }
-
-    for (let j = 0; j < 18; j++) {
-      colValue = arr[j][i];
-      colNext = arr[j + 1][i];
-
-      if (colValue && colNext && colValue === colNext) {
-        colCount = colCount + 1;
-      }
-
-      rowValue = arr[i][j];
-      rowNext = arr[i][j + 1];
-      if (rowValue && rowNext && rowValue === rowNext) {
-        rowCount = rowCount + 1;
-      }
-
-      if (colCount === 4 || rowCount === 4 || crossCount === 4) {
-        setTimeout(() => alert("!!!!!!!!!!"), 1);
-        colCount = 0;
-        rowCount = 0;
-      }
+    if (arr[x][y] === arr[b][d]) {
+      crossCount = crossCount + 1;
     }
+  }
+
+  if (yCount > 3 || xCount > 3 || crossCount > 3) {
+    return setTimeout(() => alert("!!!!!!!!!!"), 1);
   }
 };
 
@@ -78,9 +105,13 @@ function handleClick(e) {
     user = !user;
   }
 
-  check();
+  indexCheck(parseInt(row), parseInt(col));
 }
 
 if (cols) {
-  Array.from(cols).forEach((col) => col.addEventListener("click", handleClick));
+  if (game) {
+    Array.from(cols).forEach((col) =>
+      col.addEventListener("click", handleClick)
+    );
+  }
 }
